8f42e319f4349e643d8144b1790d30b68ae448c9
Anonymous275 <theanonymous275@gmail.com>
2020-03-04 18:34:06 +0200

    TCP RUDP Proxy Complete

--- proxy.cpp
+++ proxy.cpp
@@ -2,18 +2,22 @@
 //// Created by Anonymous275 on 3/3/2020.
 ////
 
+#define ENET_IMPLEMENTATION
 #include "enet.h"
 #include <WinSock2.h>
 #include <Windows.h>
 #include <WS2tcpip.h>
 #include <cstdio>
 #include <string>
+#include <thread>
 #define DEFAULT_BUFLEN 64000
 #define DEFAULT_PORT "4444"
+typedef struct {
+    ENetHost *host;
+    ENetPeer *peer;
+} Client;
 
-std::string TCPData;
 std::string RUDPData;
-std::string TCPToSend;
 std::string RUDPToSend;
 
 void TCPServerThread(){
@@ -122,13 +126,73 @@ void TCPServerThread(){
     closesocket(ClientSocket);
     WSACleanup();
 }
+void HandleEvent(ENetEvent event,Client client){
+    switch (event.type){
+        case ENET_EVENT_TYPE_CONNECT:
+            printf("Client Connected port : %u.\n",event.peer->address.port);
+            //Name Of the Server
+            event.peer->data = (void *)"Connected Server";
+            break;
+        case ENET_EVENT_TYPE_RECEIVE:
+            printf("Received: %s\n",event.packet->data);
+            RUDPData = reinterpret_cast<const char *const>(event.packet->data);
+            enet_packet_destroy (event.packet);
+            break;
+        case ENET_EVENT_TYPE_DISCONNECT:
+            printf ("%s disconnected.\n", (char *)event.peer->data);
+            // Reset the peer's client information.
+            event.peer->data = NULL;
+            break;
 
+        case ENET_EVENT_TYPE_DISCONNECT_TIMEOUT:
+            printf ("%s timeout.\n", (char *)event.peer->data);
+            event.peer->data = NULL;
+            break;
+
+        case ENET_EVENT_TYPE_NONE: break;
+    }
+}
 void RUDPClientThread(){
+    if (enet_initialize() != 0) {
+        printf("An error occurred while initializing ENet.\n");
+    }
 
+
+    Client client;
+    ENetAddress address = {0};
+
+    address.host = ENET_HOST_ANY;
+    address.port = 30814;
+
+
+    printf("starting client...\n");
+
+    enet_address_set_host(&address, "localhost");
+    client.host = enet_host_create(NULL, 1, 2, 0, 0);
+    client.peer = enet_host_connect(client.host, &address, 2, 0);
+    if (client.peer == NULL) {
+        printf("could not connect\n");
+    }
+
+    do {
+
+        ENetEvent event;
+        enet_host_service(client.host, &event, 0);
+        HandleEvent(event,client); //Handles the Events
+        if(!RUDPToSend.empty()){
+            ENetPacket* packet = enet_packet_create (RUDPToSend.c_str(),
+                                                     RUDPToSend.length()+1,
+                                                     ENET_PACKET_FLAG_RELIABLE); //Create A reliable packet using the data
+            enet_peer_send(client.peer, 0, packet);
+            RUDPToSend.clear();
+        }
+        Sleep(50);
+    } while (true);
 }
 
 
 void Start(){
-    TCPServerThread();
-    RUDPClientThread();
+    std::thread t1(TCPServerThread);
+    std::thread t2(RUDPClientThread);
+    t2.join();
 }
