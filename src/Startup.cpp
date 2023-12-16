9afdfd4d1b5c0dc7d3e240db3f175da3b3d8086f
Anonymous275 <simonas344@gmail.com>
2023-12-16 14:30:35 +0000

    v2.0.84 - add Access-Control response headers

--- src/Startup.cpp
+++ src/Startup.cpp
@@ -272,6 +272,8 @@ void PreGame(const std::string& GamePath){
 
 void set_headers(httplib::Response& res) {
     res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_header("Access-Control-Request-Method", "POST, OPTIONS, GET");
+    res.set_header("Access-Control-Request-Headers", "X-API-Version");
 }
 
 void StartProxy() {
