2d5f95cf4deaaf6e183aecca73d9245e86f3f974
Lion Kortlepel <development@kortlepel.com>
2024-03-27 16:31:11 +0100

    fix result unused attribute

--- src/Result.h
+++ src/Result.h
@@ -3,9 +3,15 @@
 #include <boost/outcome.hpp>
 #include <boost/outcome/success_failure.hpp>
 
+#ifdef WIN32
+#define BEAMMP_WARN_UNUSED
+#else
+#define BEAMMP_WARN_UNUSED __attribute__((warn_unused_result))
+#endif
+
 // attribute because [[nodiscard]] isn't possible on using decls - if you wanna write a standards proposal,
 // this is one to write.
 template<typename T, typename EC>
-using Result __attribute__((warn_unused_result)) = boost::outcome_v2::result<T, EC>;
+using Result BEAMMP_WARN_UNUSED = boost::outcome_v2::result<T, EC>;
 
 namespace outcome = boost::outcome_v2;
