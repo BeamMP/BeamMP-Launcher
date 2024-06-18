ba9719ed67e461016e283427ae4d4d5afdc28fbb
Lion Kortlepel <development@kortlepel.com>
2024-06-18 09:46:33 +0200

    fix auth packet prefix

--- src/Network/Core.cpp
+++ src/Network/Core.cpp
@@ -180,7 +180,7 @@ void Parse(std::string Data, SOCKET CSocket) {
             if (!UserRole.empty()) {
                 Auth["role"] = UserRole;
             }
-            Data = Auth.dump();
+            Data = "N" + Auth.dump();
         } else {
             Data = "N" + Login(Data.substr(Data.find(':') + 1));
         }
